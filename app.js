8e3433dc6758b9c477e66b04d749a0f554eec9fc
Paulo <8630187+papawlo@users.noreply.github.com>
2020-08-17 16:54:10 +0100

    :rocket: All done, saving urls on local storage

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 
 const relinkUrl = 'https://rel.ink/api/links/';
-const getUrl = `GET https://rel.ink/api/links/hashid`; //get url by hashid
+const getUrl = `https://rel.ink/api/links/hashid`; //get url by hashid
+
+const shortenedStorage = window.localStorage;
+let itemsArray = localStorage.getItem('items')
+    ? JSON.parse(localStorage.getItem('items'))
+    : [];
+
+localStorage.setItem('items', JSON.stringify(itemsArray));
+// const dbData = JSON.parse(localStorage.getItem('items'));
+
 
 const form = document.getElementById('form');
 const urlInput = document.getElementById('url');
@@ -8,66 +17,6 @@ const urlErrorMsg = document.getElementById('error-msg');
 const urlList = document.querySelector(".url-list");
 const copyButtons = document.querySelectorAll(".copy-btn");
 
-// Add to list of urls
-
-const addToList = (hashid, url) => {
-
-    let div = document.createElement('div');
-    div.classList.add("url-item");
-
-    // let p = document.createElement('p');
-    // let a = document.createElement('a');
-    // let b = document.createElement('button');
-    div.innerHTML = `<p>${url}</p>
-    <a href="http://rel.ink/${hashid}" class="url-link" target="_blank">http://rel.ink/${hashid}</a>
-    <button type="button" class="copy-btn btn btn-btn sm" value="http://rel.ink/${hashid}">Copy</button>`;
-    // p.innerHTML = `http://rel.ink/${url}`;
-    // a.href = `http://rel.ink/${hashid}`;
-    // a.innerHTML = `http://rel.ink/${hashid}`;
-    // b.innerHTML = 'Copy';
-    // div.append(p);
-    // div.append(a);
-    // div.append(b);
-    // console.log(div);
-    // urlList.append(div);
-    const btn = div.querySelector('.copy-btn');
-    // console.log(btn);
-    btn.className = "copied";
-    btn.innerHTML = "Copied";
-    btn.addEventListener('click', event => {
-        console.log('event added');
-
-        // var targetElement = event.target || event.srcElement;
-        copyToClipboard(event.target.value);
-    });
-    urlList.appendChild(div);
-    // console.log(urlList);
-}
-
-const copyToClipboard = str => {
-    const el = document.createElement('textarea');
-    el.value = str;
-    el.setAttribute('readonly', '');
-    el.style.position = 'absolute';
-    el.style.left = '-9999px';
-    document.body.appendChild(el);
-    el.select();
-    el.setSelectionRange(0, 99999); /*For mobile devices*/
-    document.execCommand('copy');
-    document.body.removeChild(el);
-};
-
-
-copyButtons.forEach(btn => {
-
-    btn.addEventListener('click', event => {
-        btn.classList.add("copied");
-        btn.innerHTML = "Copied";
-        // var targetElement = event.target || event.srcElement;
-        copyToClipboard(event.target.value);
-    });
-
-})
 
 urlInput.addEventListener("input", function (event) {
     // Each time the user types something, we check if the
@@ -94,29 +43,6 @@ urlInput.addEventListener("input", function (event) {
 
 });
 
-async function postUrl(url = '', data = {}) {
-    // let response = await fetch(relinkUrl, {
-    const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-            "Content-Type": "application/json; charset=UTF-8"
-        },
-        body: JSON.stringify(data)
-    }).then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return response.json();
-    }).then(data => {
-        console.log(data); // JSON data parsed by `data.json()` call
-        // console.log(data.hashid); // JSON data parsed by `data.json()` call
-
-        addToList(data.hashid, data.url);
-    }).catch(error => {
-        console.error('There has been a problem with your fetch operation:', error);
-    });;
-
-}
 form.addEventListener('submit', (event) => {
 
     // Then we prevent the form from being sent by canceling the event
@@ -133,8 +59,30 @@ form.addEventListener('submit', (event) => {
 });
 
 
+async function postUrl(url = '', payload = {}) {
+    // let response = await fetch(relinkUrl, {
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json; charset=UTF-8"
+        },
+        body: JSON.stringify(payload)
+    }).then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    }).then(data => {
+        storeData(data);
 
-function showError() {
+        addToList(data);
+    }).catch(error => {
+        console.error('There has been a problem with your fetch operation:', error);
+    });;
+
+}
+
+const showError = () => {
     if (!urlInput.value || urlInput.value.trim().length === 0) {
         // If the field is empty
         // display the following error message.
@@ -149,3 +97,66 @@ function showError() {
     urlInput.classList.add('is-invalid');
     urlErrorMsg.className = 'error-msg is-invalid';
 }
+
+// Add to list of urls
+
+const addToList = (data) => {
+    // console.log(data);
+    let { created_at, hashid, url } = data;
+
+    let div = document.createElement('div');
+    div.classList.add("url-item");
+
+    div.innerHTML = `<p title="Created at: ${created_at}">${url}</p>
+    <a href="http://rel.ink/${hashid}" class="url-link" target="_blank">http://rel.ink/${hashid}</a>
+    <button type="button" class="copy-btn btn btn-btn sm" value="http://rel.ink/${hashid}">Copy</button>`;
+
+    const btn = div.querySelector('.copy-btn');
+
+    btn.addEventListener('click', event => {
+        copyToClipboard(event.target.value);
+        btn.classList.add("copied");
+        btn.innerText = "Copied";
+    });
+    urlList.appendChild(div);
+
+}
+
+
+const storeData = data => {
+
+    itemsArray.push(data);
+    localStorage.setItem('items', JSON.stringify(itemsArray));
+}
+
+itemsArray.forEach((item) => {
+    addToList(item);
+})
+const copyToClipboard = str => {
+    const el = document.createElement('textarea');
+    el.value = str;
+    el.setAttribute('readonly', '');
+    el.style.position = 'absolute';
+    el.style.left = '-9999px';
+    document.body.appendChild(el);
+    el.select();
+    el.setSelectionRange(0, 99999); /*For mobile devices*/
+    document.execCommand('copy');
+    document.body.removeChild(el);
+};
+
+
+const navMobile = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav');
+    // const navLinks = document.querySelectorAll('.nav__links li');
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+
+        // Burger Animation
+        burger.classList.toggle('toggle');
+    });
+}
+
+navMobile();
